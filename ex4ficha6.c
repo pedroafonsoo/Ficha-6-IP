36851935f6e4c908e6c79a3c7b19e3fb6e072938
pedroafonsoo <carlospmafonso@hotmail.com>
2016-01-01 18:41:50 +0000

    exercicio 4 da ficha 6

--- ex4ficha6.c
+++ ex4ficha6.c
@@ -1,7 +1,19 @@
 #include<stdio.h>
+#define N 100
 
+int compara(int vetor1[N],int vetor2[N]){
+    int i,verifica;
+    verifica=0;
 
-void main(){
+    for(i=0;i<N;i++){
+        if(vetor1[i] == vetor2[i]){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+
 
 
 
