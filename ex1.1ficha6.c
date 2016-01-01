bae771a7edeb9677d64c7f21ff24b0670fb41df2
pedroafonsoo <carlospmafonso@hotmail.com>
2016-01-01 17:27:10 +0000

    ex 1.1 da ficha 6

---
+++ ex1.1ficha6.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+#define N 6
+
+void main(){
+        int i,num,vetor[N],soma,media;
+        soma=0;
+
+        for(i=0;i<N;i++){
+            printf("Introduza um numero \n");
+            scanf("%d",&num);
+            num=vetor[i];
+        }
+
+        for(i=0;i<N;i++){
+
+            soma=soma+vetor[i];
+
+        }
+         media=vetor[i]/num;
+         printf("O calculo da media deu: %d",media);
+        }
